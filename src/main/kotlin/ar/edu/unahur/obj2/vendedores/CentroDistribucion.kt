cca2bb5ddce43cbe8852d56e0ef1bb69a31a86ad
FrancoRueta <askjfuhje@hotmail.com>
2021-04-05 17:54:17 -0300

    Incompleto, problema con los package.

---
+++ src/main/kotlin/ar/edu/unahur/obj2/vendedores/CentroDistribucion.kt
@@ -0,0 +1,18 @@
+package ar.edu.unahur.obj2.vendedores
+import ar.edu.unahur.obj2.vendedores.
+
+
+
+
+
+
+
+
+
+
+
+
+class CentroDistribucaion(val ciudadOrigen: Ciudad, val vendedor: Vendedor){
+
+}
+
--- src/main/kotlin/ar/edu/unahur/obj2/vendedores/Vendedor.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/vendedores/Vendedor.kt
@@ -1,51 +1,75 @@
 package ar.edu.unahur.obj2.vendedores
 
-class Certificacion(val esDeProducto: Boolean, val puntaje: Int)
+class Certificacion(val esDeProducto: Boolean, val puntaje: Int) //Clase
 
 abstract class Vendedor {
-  // Acá es obligatorio poner el tipo de la lista, porque como está vacía no lo puede inferir.
-  // Además, a una MutableList se le pueden agregar elementos
-  val certificaciones = mutableListOf<Certificacion>()
+    // Acá es obligatorio poner el tipo de la lista, porque como está vacía no lo puede inferir.
+    // Además, a una MutableList se le pueden agregar elementos
+    val certificaciones = mutableListOf<Certificacion>() //Lista de certificaciones
 
-  // Definimos el método abstracto.
-  // Como no vamos a implementarlo acá, es necesario explicitar qué devuelve.
-  abstract fun puedeTrabajarEn(ciudad: Ciudad): Boolean
+    // Definimos el método abstracto.
+    // Como no vamos a implementarlo acá, es necesario explicitar qué devuelve.
+    abstract fun puedeTrabajarEn(ciudad: Ciudad): Boolean //Tipo bool
 
-  // En las funciones declaradas con = no es necesario explicitar el tipo
-  fun esVersatil() =
-    certificaciones.size >= 3
-      && this.certificacionesDeProducto() >= 1
-      && this.otrasCertificaciones() >= 1
+    // En las funciones declaradas con = no es necesario explicitar el tipo
+    fun esVersatil() = //Retorna bool
+            certificaciones.size >= 3
+                    && this.certificacionesDeProducto() >= 1
+                    && this.otrasCertificaciones() >= 1
 
-  // Si el tipo no está declarado y la función no devuelve nada, se asume Unit (es decir, vacío)
-  fun agregarCertificacion(certificacion: Certificacion) {
-    certificaciones.add(certificacion)
-  }
+    // Si el tipo no está declarado y la función no devuelve nada, se asume Unit (es decir, vacío)
+    fun agregarCertificacion(certificacion: Certificacion) { //No retorna
+        certificaciones.add(certificacion)
+    }
 
-  fun esFirme() = this.puntajeCertificaciones() >= 30
+    fun esFirme() = this.puntajeCertificaciones() >= 30 //Tipo Bool
 
-  fun certificacionesDeProducto() = certificaciones.count { it.esDeProducto }
-  fun otrasCertificaciones() = certificaciones.count { !it.esDeProducto }
+    fun certificacionesDeProducto() = certificaciones.count { it.esDeProducto } //Tipo Int
+    fun otrasCertificaciones() = certificaciones.count { !it.esDeProducto } //Tipo Int
 
-  fun puntajeCertificaciones() = certificaciones.sumBy { c -> c.puntaje }
+    fun puntajeCertificaciones() = certificaciones.sumBy { c -> c.puntaje } //Tipo Int
+
+    //Agregado debido al punto Vendedor Influyente, el cual nos pide como caracteristica que
+    //los vendedores puedan o no ser "influyentes", el cual cambia acorde a cada uno de ellos.
+    abstract fun esInfluyente(): Boolean //Tipo Bool
+
+    abstract fun puedeTrabajarEn(ciudad: Ciudad): Boolean
 }
 
 // En los parámetros, es obligatorio poner el tipo
 class VendedorFijo(val ciudadOrigen: Ciudad) : Vendedor() {
-  override fun puedeTrabajarEn(ciudad: Ciudad): Boolean {
-    return ciudad == ciudadOrigen
-  }
+    override fun puedeTrabajarEn(ciudad: Ciudad): Boolean {  //Tipo Bool
+        return ciudad == ciudadOrigen
+    }
+    override fun esInfluyente() = false //Tipo Bool
+
 }
 
 // A este tipo de List no se le pueden agregar elementos una vez definida
 class Viajante(val provinciasHabilitadas: List<Provincia>) : Vendedor() {
-  override fun puedeTrabajarEn(ciudad: Ciudad): Boolean {
-    return provinciasHabilitadas.contains(ciudad.provincia)
-  }
+    override fun puedeTrabajarEn(ciudad: Ciudad): Boolean { //Tipo Bool
+        return provinciasHabilitadas.contains(ciudad.provincia)
+    }
+
+    override fun esInfluyente(): Boolean { //Tipo Bool
+        return (
+                (this.provinciasHabilitadas.sumBy { provincia -> provincia.poblacion  }) >= 10000000
+                )
+    }
 }
 
-class ComercioCorresponsal(val ciudades: List<Ciudad>) : Vendedor() {
-  override fun puedeTrabajarEn(ciudad: Ciudad): Boolean {
-    return ciudades.contains(ciudad)
-  }
+class ComercioCorresponsal(val ciudades: List<Ciudad>) : Vendedor() { //Tipo Vendedor
+    override fun puedeTrabajarEn(ciudad: Ciudad): Boolean { //tipo bool
+        return ciudades.contains(ciudad)
+    }
+
+    override fun esInfluyente(): Boolean { //Tipo Bool
+        return ((this.ciudades.size) >= 5 || (ciudades.map { ciudad -> ciudad.provincia }).size >= 3)
+    }
 }
+
+
+class CentroDistribucion(val ciudadOrigen: Ciudad,vendedores: Vendedor){
+
+}
+
